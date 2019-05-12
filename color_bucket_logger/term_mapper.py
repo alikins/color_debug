9ff778fcac8cd8a52b3417a5fec05870a7444622
Adrian Likins <alikins@redhat.com>
2019-05-12 14:14:10 -0400

    Exp with a Palette object
    mv the index color palette creation stuff to its
    own class

--- color_bucket_logger/term_mapper.py
+++ color_bucket_logger/term_mapper.py
@@ -1,5 +1,47 @@
 from . import mapper
 
+class DarkTermPalette(object):
+    '''An indexed color palette, ie, an ordered list of strings of terminal color escape codes'''
+
+    # hacky ansi color stuff
+    RESET_SEQ = "\033[0m"
+    COLOR_SEQ = "\033[1;%dm"
+    BOLD_SEQ = "\033[1m"
+
+    NUMBER_OF_BASE_COLORS = 8
+
+    # NUMBER_OF_COLORS = 216
+    # the xterm256 colors 0-8 and 8-16 are normal and bright term colors, 16-231 is from a 6x6x6 rgb cube
+    # 232-255 are the grays (white to gray to black)
+
+    RGB_COLOR_OFFSET = 16 + 2
+    # +2 is to skip black and very dark blue as valid color.
+
+    # FIXME: needs to learn to support dark/light themes
+    START_OF_COLORS = RGB_COLOR_OFFSET
+    END_OF_COLORS = 231
+    NUMBER_OF_COLORS = END_OF_COLORS - RGB_COLOR_OFFSET
+
+    BASE_COLORS = dict((color_number, color_seq) for
+                       (color_number, color_seq) in [(x, "\033[38;5;%dm" % (x)) for x in range(NUMBER_OF_BASE_COLORS)])
+    # \ x 1 b [ 38 ; 5; 231m
+    THREAD_COLORS = dict((color_number, color_seq) for
+                         (color_number, color_seq) in [(x, "\033[38;5;%dm" % x) for x in range(START_OF_COLORS, END_OF_COLORS)])
+    ALL_COLORS = {}
+    ALL_COLORS.update(BASE_COLORS)
+    # ALL_COLORS.update(THREAD_COLORS)
+
+    # import sys
+    # for x in ALL_COLORS:
+    #    sys.stdout.write("%s: %sX - X - X - X - X - X%s\n" % (x, ALL_COLORS[x], RESET_SEQ))
+    BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE = BASE_COLORS.keys()
+
+    DEFAULT_COLOR_IDX = 257
+    RESET_SEQ_IDX = 256
+    ALL_COLORS[RESET_SEQ_IDX] = RESET_SEQ
+
+    DEFAULT_COLOR = WHITE
+    ALL_COLORS[DEFAULT_COLOR_IDX] = ALL_COLORS[DEFAULT_COLOR]
 
 class TermColorMapper(mapper.BaseColorMapper):
     # hacky ansi color stuff
